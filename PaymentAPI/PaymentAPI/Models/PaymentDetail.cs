0c42e63791550ee70d24c2b36e93f32d1bf0fdd2
EmilMind <emil.mindubaev@innopolis.ru>
2021-06-01 15:37:35 +0300

    first commit

---
+++ PaymentAPI/PaymentAPI/Models/PaymentDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetail
+    {
+        public int PaymentDetailId { get; set; }
+        public string CardOwnerName { get; set; }
+        public string CardNumber { get; set; }
+        public string ExpirationDate { get; set; }
+        public string SecurityCode { get; set; }
+    }
+}
